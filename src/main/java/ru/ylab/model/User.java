ecfccca9558e43b569a66ac46427d750b2015fe1
Evgeny Bayandin <5087081@gmail.com>
2024-06-27 22:34:52 +0300

    Update User.java

--- src/main/java/ru/ylab/model/User.java
+++ src/main/java/ru/ylab/model/User.java
@@ -1,38 +1,47 @@
 package ru.ylab.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private int id;
     private String username;
     private String password;
     private boolean isAdmin;
-
-    public User(String username, String password, boolean isAdmin) {
-        this.username = username;
-        this.password = password;
-        this.isAdmin = isAdmin;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isAdmin() {
-        return isAdmin;
-    }
-
-    public void setAdmin(boolean admin) {
-        isAdmin = admin;
-    }
 }
+
+//    public User(String username, String password, boolean isAdmin) {
+//        this.username = username;
+//        this.password = password;
+//        this.isAdmin = isAdmin;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public boolean isAdmin() {
+//        return isAdmin;
+//    }
+//
+//    public void setAdmin(boolean admin) {
+//        isAdmin = admin;
+//    }
+//}
 
