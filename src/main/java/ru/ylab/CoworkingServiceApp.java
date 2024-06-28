1ec4e6f1a76905050732a53077002a75b34cf281
Evgeny Bayandin <5087081@gmail.com>
2024-06-28 10:48:00 +0300

    Update CoworkingServiceApp

--- src/main/java/ru/ylab/CoworkingServiceApp.java
+++ src/main/java/ru/ylab/CoworkingServiceApp.java
@@ -2,6 +2,7 @@ package ru.ylab;
 
 import java.util.Scanner;
 import ru.ylab.in.InputReader;
+import ru.ylab.util.DatabaseManager;
 import ru.ylab.out.OutputWriter;
 import ru.ylab.repository.BookingRepository;
 import ru.ylab.repository.ResourceRepository;
@@ -14,25 +15,30 @@ import ru.ylab.ui.ConsoleUI;
 
 public class CoworkingServiceApp {
     public static void main(String[] args) {
+        try {
+            DatabaseManager.initializeDatabase();
+            Scanner scanner = new Scanner(System.in);
+            InputReader inputReader = new InputReader(scanner);
+            OutputWriter outputWriter = new OutputWriter();
 
-        Scanner scanner = new Scanner(System.in);
-        InputReader inputReader = new InputReader(scanner);
-        OutputWriter outputWriter = new OutputWriter();
+            UserRepository userRepository = new UserRepository(new DatabaseManager() {
+            });
+            ResourceRepository resourceRepository = new ResourceRepository();
+            BookingRepository bookingRepository = new BookingRepository();
 
-        UserRepository userRepository = new UserRepository();
-        ResourceRepository resourceRepository = new ResourceRepository();
-        BookingRepository bookingRepository = new BookingRepository();
+            UserService userService = new UserService(userRepository);
+            BookingService bookingService = new BookingService(bookingRepository);
+            ResourceService resourceService = new ResourceService(resourceRepository, bookingService);
+            AuthenticationService authenticationService = new AuthenticationService(userRepository);
 
-        UserService userService = new UserService(userRepository);
-        BookingService bookingService = new BookingService(bookingRepository);
-        ResourceService resourceService = new ResourceService(resourceRepository, bookingService);
-        AuthenticationService authenticationService = new AuthenticationService(userRepository);
+            outputWriter.printLine("Welcome to Coworking service!");
 
-        outputWriter.printLine("Welcome to Coworking service!");
+            ConsoleUI consoleUI = new ConsoleUI(inputReader, outputWriter, userService, resourceService, bookingService, authenticationService);
 
-        ConsoleUI consoleUI = new ConsoleUI(inputReader, outputWriter, userService, resourceService, bookingService, authenticationService);
-
-        consoleUI.run();
-
+            consoleUI.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
